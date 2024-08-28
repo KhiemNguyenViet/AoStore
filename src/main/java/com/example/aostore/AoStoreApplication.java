f953b4e851c2df9d79b263b258080af0d68925ff
ADMIN <khiemnguyenviet.2004@gmail.com>
2024-08-28 22:45:47 +0700

    init project

---
+++ src/main/java/com/example/aostore/AoStoreApplication.java
@@ -0,0 +1,14 @@
+package com.example.aostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AoStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AoStoreApplication.class, args);
+        System.out.println("hello,git!");
+    }
+
+}
---
+++ src/test/java/com/example/aostore/AoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aostore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AoStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
